99e546b8b271c2ab97ca57fe20c74292e1383de6
Scott Kirkland <srkirkland@gmail.com>
2011-02-28 17:17:46 +0000

    Moved the route registration out of the global asax file
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Agribusiness/Trunk/Agribusiness;C9485

--- Agribusiness.Web/Global.asax.cs
+++ Agribusiness.Web/Global.asax.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
-using System.Web.Routing;
 using Agribusiness.Core;
 using Agribusiness.Web.Controllers;
 using Castle.Windsor;
@@ -11,35 +7,22 @@ using Microsoft.Practices.ServiceLocation;
 using UCDArch.Data.NHibernate;
 using UCDArch.Web.IoC;
 using UCDArch.Web.ModelBinder;
-using UCDArch.Web.Validator;
 
 namespace Agribusiness.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                "Default",                                              // Route name
-                "{controller}/{action}/{id}",                           // URL with parameters
-                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
-            );
-
-        }
-
         protected void Application_Start()
         {
 #if DEBUG
             HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
 #endif
 
-            RegisterRoutes(RouteTable.Routes);
+            RouteConfigurator.RegisterRoutes();
 
             ModelBinders.Binders.DefaultBinder = new UCDArchModelBinder();
 
-           NHibernateSessionConfiguration.Mappings.UseFluentMappings(typeof(Class1).Assembly);
+            NHibernateSessionConfiguration.Mappings.UseFluentMappings(typeof(Class1).Assembly);
 
             IWindsorContainer container = InitializeServiceLocator();
         }
