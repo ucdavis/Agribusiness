36eb5116c8f790c5a2ca48b9f370548035bc1c65
Scott Kirkland <srkirkland@gmail.com>
2011-03-04 19:52:31 +0000

    Added some nhibernate references, fixed up the web.config and wrote a sample page for dynamically loading profile images.
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Agribusiness/Trunk/Agribusiness;C9535

--- Agribusiness.Web/Controllers/HomeController.cs
+++ Agribusiness.Web/Controllers/HomeController.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using Agribusiness.Core.Domain;
 using UCDArch.Web.Attributes;
+using System;
 
 namespace Agribusiness.Web.Controllers
 {
@@ -20,6 +19,24 @@ namespace Agribusiness.Web.Controllers
         public ActionResult About()
         {
             return View();
+        }
+
+        [Transaction]
+        public ActionResult LoadImages()
+        {
+            var people = Repository.OfType<Person>().GetAll();
+
+            return View(people.First());
+        }
+
+        [Transaction]
+        public ActionResult LoadPicture(int id)
+        {
+            var person = Repository.OfType<Person>().GetById(id);
+
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            return File(person.Picture, "image/jpg");
         }
     }
 }
