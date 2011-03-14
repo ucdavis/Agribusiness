1160d461728cf7abe690f96bdff90f73b94a0df5
Lai, Alan <anlai@ucdavis.edu>
2011-03-14 22:13:22 +0000

    added role filter added seminar controller updated template for controller
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Agribusiness/Trunk/Agribusiness;C9618

---
+++ Agribusiness.Web/Controllers/Filters/RoleFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Agribusiness.Web.Controllers.Filters
+{
+    public class UserOnlyAttribute : AuthorizeAttribute
+    {
+        public UserOnlyAttribute()
+        {
+            Roles = RoleNames.User;
+        }
+    }
+
+    public static class RoleNames
+    {
+        public static readonly string User = "User";
+    }
+}
---
+++ Agribusiness.Web/Controllers/SeminarController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Web.Mvc;
+using Agribusiness.Core.Domain;
+using Agribusiness.Web.Controllers.Filters;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Web.Controller;
+using UCDArch.Web.Helpers;
+using UCDArch.Core.Utils;
+
+namespace Agribusiness.Web.Controllers
+{
+    /// <summary>
+    /// Controller for the Seminar class
+    /// </summary>
+    public class SeminarController : ApplicationController
+    {
+	    private readonly IRepository<Seminar> _seminarRepository;
+
+        public SeminarController(IRepository<Seminar> seminarRepository)
+        {
+            _seminarRepository = seminarRepository;
+        }
+    
+        //
+        // GET: /Seminar/
+        [UserOnly]
+        public ActionResult Index()
+        {
+            var seminarList = _seminarRepository.Queryable;
+
+            return View(seminarList);
+        }
+
+        [UserOnly]
+        public ActionResult Create()
+        {
+            var viewModel = SeminarViewModel.Create(Repository);
+
+            return View(viewModel);
+        }
+
+    }
+
+	/// <summary>
+    /// ViewModel for the Seminar class
+    /// </summary>
+    public class SeminarViewModel
+	{
+		public Seminar Seminar { get; set; }
+ 
+		public static SeminarViewModel Create(IRepository repository, Seminar seminar = null)
+		{
+			Check.Require(repository != null, "Repository must be supplied");
+			
+			var viewModel = new SeminarViewModel {Seminar = seminar ?? new Seminar()};
+ 
+			return viewModel;
+		}
+	}
+}
