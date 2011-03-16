f3f4cba9036c12c0f60ef6b5d753b15a42d2df27
Lai, Alan <anlai@ucdavis.edu>
2011-03-16 22:50:53 +0000

    added scott's addition to fluent html to get validation.
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Agribusiness/Trunk/Agribusiness;C9640

---
+++ Agribusiness.Web/Helpers/FluentHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Agribusiness.Web.Helpers
+{
+    public static class FluentHtmlExtensions
+    {
+        public static T IncludeUnobtrusiveValidationAttributes<T>(this T element, HtmlHelper htmlHelper)
+            where T : MvcContrib.FluentHtml.Elements.IElement
+        {
+            IDictionary<string, object> validationAttributes = htmlHelper
+                .GetUnobtrusiveValidationAttributes(element.GetAttr("name"));
+
+            foreach (var validationAttribute in validationAttributes)
+            {
+                element.SetAttr(validationAttribute.Key, validationAttribute.Value);
+            }
+
+            return element;
+
+        }
+
+    }
+
+
+}
