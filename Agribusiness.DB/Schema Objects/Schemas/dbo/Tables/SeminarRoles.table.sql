4c1e8902fc226679b53cae7dc4d54a006b7ba46a
Alan Lai <anlai@ucdavis.edu>
2011-04-22 10:21:23 -0700

    added a description to the seminar roles in the db.

--- Agribusiness.DB/Schema Objects/Schemas/dbo/Tables/SeminarRoles.table.sql
+++ Agribusiness.DB/Schema Objects/Schemas/dbo/Tables/SeminarRoles.table.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[SeminarRoles] (
-    [id]       CHAR (2)        NOT NULL,
-    [Name]     VARCHAR (50)    NOT NULL,
-    [Discount] DECIMAL (10, 2) NULL
+    [id]          CHAR (2)        NOT NULL,
+    [Name]        VARCHAR (50)    NOT NULL,
+    [Discount]    DECIMAL (10, 2) NULL,
+    [Description] VARCHAR (200)   NULL
 );
+
+
 
