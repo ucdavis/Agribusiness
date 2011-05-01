2516c78687407779000eb6ae0cd835f2ecb985c0
Alan Lai <anlai@ucdavis.edu>
2011-05-01 08:44:04 -0700

    added code that imports archived contact firms

--- Agribusiness.Import/Controllers/ContactFirmsController.cs
+++ Agribusiness.Import/Controllers/ContactFirmsController.cs
@@ -59,5 +59,53 @@ namespace Agribusiness.Import.Controllers
             return View(viewModel);
         }
 
+        public ActionResult ArchiveContactFirms()
+        {
+            var imported = Db.Trackings.Where(a => a.Name == _rContactFirm).Any();
+
+            var cfirms = new List<ContactFirms>();
+            var errors = new List<KeyValuePair<string, string>>();
+
+            var sheet = ExcelHelpers.OpenWorkbook(Server.MapPath("~/Assets/archived_R_Conatct_Firm.xls"));
+
+            for (var i = sheet.FirstRowNum + 1; i <= sheet.LastRowNum; i++)
+            {
+                var row = sheet.GetRow(i);
+
+                try
+                {
+                    var rcfId = ExcelHelpers.ReadIntCell(row, 2);
+                    if (Db.ContactFirms.Any(a => a.rcfId == rcfId)) throw new Exception("Already exists");
+
+                    var cfirm = new ContactFirms();
+
+                    cfirm.ContactId = ExcelHelpers.ReadIntCell(row, 0);
+                    cfirm.FirmId = ExcelHelpers.ReadIntCell(row, 1);
+                    cfirm.rcfId = ExcelHelpers.ReadIntCell(row, 2);
+
+                    if (!imported)
+                    {
+                        Db.ContactFirms.Add(cfirm);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    errors.Add(new KeyValuePair<string, string>(row.GetCell(13).ToString(), ex.Message));
+                }
+
+            }
+
+            if (!imported)
+            {
+                var tracking = new Tracking() { Name = _rContactFirm };
+                Db.Trackings.Add(tracking);
+            }
+
+            Db.SaveChanges();
+
+            var viewModel = ContactFirmViewModel.Create(cfirms, errors, imported);
+            return View(viewModel);
+        }
     }
 }
