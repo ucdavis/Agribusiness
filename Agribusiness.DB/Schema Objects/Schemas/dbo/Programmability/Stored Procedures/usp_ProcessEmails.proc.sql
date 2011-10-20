c93fba396caa11cb10b7ff521315884e4f327497
Alan Lai <anlai@ucdavis.edu>
2011-10-20 09:40:39 -0700

    tossed in some email handling stuff in the db.

---
+++ Agribusiness.DB/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/usp_ProcessEmails.proc.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[usp_ProcessEmails]
+
+AS
+
+declare @cursor cursor
+
+declare @id int, @subject varchar(100), @body varchar(max), @from varchar(50), @email varchar(max)
+
+set @cursor = cursor for
+	select id, subject, body, fromaddress, email from vEmailQueue
+	
+open @cursor
+
+fetch next from @cursor into @id, @subject, @body, @from, @email
+
+while(@@FETCH_STATUS = 0)
+begin
+
+	exec msdb.dbo.sp_send_dbmail
+		@from_address = 'agribusiness@ucdavis.edu',
+		@reply_to = 'agribusiness@ucdavis.edu',
+		@recipients = @email,
+		@subject = @subject,
+		@body = @body,
+		@body_format= 'HTML'
+
+	update emailqueue set pending = 0, SentDateTime = getdate() where id = @id
+
+	fetch next from @cursor into @id, @subject, @body, @from, @email
+
+end
+
+close @cursor
+deallocate @cursor
+
+
+
+RETURN 0
---
+++ Agribusiness.DB/Schema Objects/Schemas/dbo/Views/vEmailQueue.view.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vEmailQueue]
+	AS 
+
+	SELECT     eq.id, eq.Subject, eq.Body, ISNULL(eq.FromAddress, 'agribusiness@ucdavis.edu') AS fromaddress, 
+                      CASE WHEN communicationoptionid = 'DR' THEN u.loweredusername WHEN communicationoptionid = 'CA' THEN u.loweredusername + ',' + c.email WHEN communicationoptionid
+                       = 'AS' THEN c.email END AS email
+FROM         dbo.EmailQueue AS eq INNER JOIN
+                      dbo.People AS p ON eq.PersonId = p.id INNER JOIN
+                      dbo.aspnet_Users AS u ON p.UserId = u.UserId LEFT OUTER JOIN
+                      dbo.Contacts AS c ON c.PersonId = p.id AND c.ContactTypeId = 'A'
+WHERE     (eq.Pending = 1)
