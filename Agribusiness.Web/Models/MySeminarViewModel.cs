1a111bdfca99c6b7599003239948221d99b438e0
Alan Lai <anlai@ucdavis.edu>
2011-11-15 20:17:55 -0800

    added a new link generator for crp link with coupon code.

--- Agribusiness.Web/Models/MySeminarViewModel.cs
+++ Agribusiness.Web/Models/MySeminarViewModel.cs
@@ -19,9 +19,18 @@ namespace Agribusiness.Web.Models
             var viewModel = new MySeminarViewModel()
                                 {
                                     SeminarPerson =  seminarPerson,
-                                    Seminar = seminarPerson.Seminar,
-                                    CrpLink = string.Format(ConfigurationManager.AppSettings["CrpLink"], seminarPerson.Seminar.RegistrationId, seminarPerson.ReferenceId)
+                                    Seminar = seminarPerson.Seminar
                                 };
+
+            // if it has a coupon code, then add it in
+            if (!string.IsNullOrWhiteSpace(seminarPerson.CouponCode))
+            {
+                viewModel.CrpLink = string.Format(ConfigurationManager.AppSettings["crplinkCoupon"], seminarPerson.Seminar.RegistrationId, seminarPerson.ReferenceId, seminarPerson.CouponCode);
+            }
+            else
+            {
+                viewModel.CrpLink = string.Format(ConfigurationManager.AppSettings["CrpLink"], seminarPerson.Seminar.RegistrationId, seminarPerson.ReferenceId);
+            }
 
             return viewModel;
         }
