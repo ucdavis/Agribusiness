4ec7ed7b7738657b8e5e90cbb7cd54b2dec39ec6
Alan Lai <anlai@ucdavis.edu>
2011-12-08 08:23:34 -0800

    adding page to create an entire profile from an information request.

--- Agribusiness.Web/Controllers/InformationRequestController.cs
+++ Agribusiness.Web/Controllers/InformationRequestController.cs
@@ -1,7 +1,10 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using Agribusiness.Core.Domain;
+using Agribusiness.Web.App_GlobalResources;
 using Agribusiness.Web.Controllers.Filters;
+using Agribusiness.Web.Models;
+using Agribusiness.Web.Services;
 using AutoMapper;
 using Resources;
 using UCDArch.Core.PersistanceSupport;
@@ -19,11 +22,15 @@ namespace Agribusiness.Web.Controllers
     {
 	    private readonly IRepository<InformationRequest> _informationrequestRepository;
         private readonly IRepository<InformationRequestNote> _informationRequestNoteRepository;
+        private readonly IRepositoryWithTypedId<AddressType, char> _addressTypeRepository;
+        private readonly IFirmService _firmService;
 
-        public InformationRequestController(IRepository<InformationRequest> informationrequestRepository, IRepository<InformationRequestNote> informationRequestNoteRepository)
+        public InformationRequestController(IRepository<InformationRequest> informationrequestRepository, IRepository<InformationRequestNote> informationRequestNoteRepository, IRepositoryWithTypedId<AddressType, char> addressTypeRepository, IFirmService firmService)
         {
             _informationrequestRepository = informationrequestRepository;
             _informationRequestNoteRepository = informationRequestNoteRepository;
+            _addressTypeRepository = addressTypeRepository;
+            _firmService = firmService;
         }
 
         //
@@ -112,6 +119,63 @@ namespace Agribusiness.Web.Controllers
             
 
             return View(irn);
+        }
+
+        /// <summary>
+        /// Create an account from Information Request
+        /// </summary>
+        /// <param name="id">Information Request Id</param>
+        /// <returns></returns>
+        public ActionResult CreatePerson(int id)
+        {
+            var ir = _informationrequestRepository.GetNullableById(id);
+
+            if (ir == null)
+            {
+                Message = string.Format(Messages.NotFound, "information request", id);
+                return this.RedirectToAction(a => a.Index());
+            }
+
+            var viewModel = PersonViewModel.Create(Repository, _firmService);
+
+            // set the person's information
+            var firstname = ir.Name.Trim().Substring(0, ir.Name.LastIndexOf(' '));
+            var lastname = ir.Name.Trim().Substring(ir.Name.LastIndexOf(' '));
+
+            viewModel.Person.FirstName = firstname.Trim();
+            viewModel.Person.LastName = lastname.Trim();
+            viewModel.Email = ir.Email;
+            viewModel.UserName = string.Format("{0}.{1}", firstname.Trim(), lastname.Trim());
+
+            // fake phone number
+            viewModel.Person.Phone = "555-555-5555";    
+
+            // fake address since we don't have it yet
+            var atype = _addressTypeRepository.GetNullableById((char)StaticIndexes.Address_Business[0]);
+
+            var address = viewModel.Addresses.Where(a => a.AddressType == atype).FirstOrDefault();
+            address.Line1 = "Please Fill Address";
+            address.Zip = "Please Fill Zip Code";
+            
+            // see if we can extract city/state out of the information reuqest
+            var commaIndex = ir.Location.IndexOf(',');
+
+            // no comma, probably no state information
+            if (commaIndex < 0)
+            {
+                address.City = ir.Location;
+            }
+            // comma exists, most likely a state exists
+            else
+            {
+                address.City = ir.Location.Substring(0, commaIndex).Trim();
+                address.State = ir.Location.Substring(commaIndex + 1).Trim();
+            }
+            
+            //var address = new Address("Please Fill In", string.Empty, "Please Fill", "CA", "55555", atype, viewModel.Person);
+            //viewModel.Addresses.Add(address);
+
+            return View(viewModel);
         }
 
     }
