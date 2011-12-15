a78b697a997beb44ae8feecfcdabbfee6b7e1b00
Alan Lai <anlai@ucdavis.edu>
2011-12-15 07:45:46 -0800

    tweaking the crp address.

--- Agribusiness.Web/Models/MySeminarViewModel.cs
+++ Agribusiness.Web/Models/MySeminarViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Configuration;
+using System.Linq;
 using Agribusiness.Core.Domain;
+using Agribusiness.Web.App_GlobalResources;
 using UCDArch.Core.PersistanceSupport;
 using UCDArch.Core.Utils;
 
@@ -22,14 +24,28 @@ namespace Agribusiness.Web.Models
                                     Seminar = seminarPerson.Seminar
                                 };
 
+            // load business address
+            var address = seminarPerson.Person.Addresses.Where(a => a.AddressType.Id == StaticIndexes.Address_Business[0]).FirstOrDefault();
+
+            address = address ?? new Address();
+
             // if it has a coupon code, then add it in
             if (!string.IsNullOrWhiteSpace(seminarPerson.CouponCode))
             {
-                viewModel.CrpLink = string.Format(ConfigurationManager.AppSettings["crplinkCoupon"], seminarPerson.Seminar.RegistrationId, seminarPerson.ReferenceId, seminarPerson.CouponCode);
+                viewModel.CrpLink = string.Format(ConfigurationManager.AppSettings["crplinkCoupon"], 
+                    seminarPerson.Seminar.RegistrationId, seminarPerson.ReferenceId, seminarPerson.Seminar.RegistrationPassword,
+                    seminarPerson.CouponCode,
+                    seminarPerson.Person.FirstName, seminarPerson.Person.LastName,
+                    address.Line1, address.Line2, address.City, address.State, address.Zip,
+                    seminarPerson.Person.Phone, seminarPerson.Person.User.Email);
             }
             else
             {
-                viewModel.CrpLink = string.Format(ConfigurationManager.AppSettings["CrpLink"], seminarPerson.Seminar.RegistrationId, seminarPerson.ReferenceId);
+                viewModel.CrpLink = string.Format(ConfigurationManager.AppSettings["CrpLink"],
+                    seminarPerson.Seminar.RegistrationId, seminarPerson.ReferenceId, seminarPerson.Seminar.RegistrationPassword,
+                    seminarPerson.Person.FirstName, seminarPerson.Person.LastName,
+                    address.Line1, address.Line2, address.City, address.State, address.Zip,
+                    seminarPerson.Person.Phone, seminarPerson.Person.User.Email);
             }
 
             return viewModel;
