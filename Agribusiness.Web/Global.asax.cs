9c6b0c0f753eb7e5b3032465b2f14701560c8075
Scott Kirkland <srkirkland@gmail.com>
2012-07-18 15:35:56 -0700

    removed existing bins and reinstalled via nuget

--- Agribusiness.Web/Global.asax.cs
+++ Agribusiness.Web/Global.asax.cs
@@ -1,15 +1,6 @@
 ﻿using System.Web;
-using System.Web.Mvc;
-using Agribusiness.Core;
-using Agribusiness.Core.Domain;
-using Agribusiness.Web.Controllers;
 using Agribusiness.Web.Helpers;
-using Castle.Windsor;
 using Elmah;
-using Microsoft.Practices.ServiceLocation;
-using UCDArch.Data.NHibernate;
-using UCDArch.Web.IoC;
-using UCDArch.Web.ModelBinder;
 
 namespace Agribusiness.Web
 {
@@ -17,33 +8,9 @@ namespace Agribusiness.Web
     {
         protected void Application_Start()
         {
-#if DEBUG
-            HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
-#endif
-
             RouteConfigurator.RegisterRoutes();
 
-            ModelBinders.Binders.DefaultBinder = new UCDArchModelBinder();
-
-            NHibernateSessionConfiguration.Mappings.UseFluentMappings(typeof(AddressType).Assembly);
-
-            IWindsorContainer container = InitializeServiceLocator();
-
             AutomapperConfig.Configure();
-        }
-
-        private static IWindsorContainer InitializeServiceLocator()
-        {
-            IWindsorContainer container = new WindsorContainer();
-
-            ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
-
-            container.RegisterControllers(typeof(HomeController).Assembly);
-            ComponentRegistrar.AddComponentsTo(container);
-
-            ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
-
-            return container;
         }
 
         void ErrorLog_Filtering(object sender, ExceptionFilterEventArgs args)
