5a155ed04cf7c269789ba14a5a9b5bbfd77a8aae
Alan Lai <anlai@ucdavis.edu>
2012-09-11 11:12:54 -0700

    fixed some issues with template editing/details

--- Agribusiness.Core/Domain/Template.cs
+++ Agribusiness.Core/Domain/Template.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using FluentNHibernate.Mapping;
 using UCDArch.Core.DomainModel;
 
@@ -9,6 +10,7 @@ namespace Agribusiness.Core.Domain
         public Template()
         {
             IsActive = true;
+            DateCreated = DateTime.Now;
         }
 
         [Required]
@@ -18,6 +20,8 @@ namespace Agribusiness.Core.Domain
         public virtual NotificationType NotificationType { get; set; }
         [Required]
         public virtual Seminar Seminar { get; set; }
+
+        public virtual DateTime DateCreated { get; set; }
     }
 
     public class TemplateMap : ClassMap<Template>
@@ -30,6 +34,7 @@ namespace Agribusiness.Core.Domain
             Map(x => x.IsActive);
             References(x => x.NotificationType);
             References(x => x.Seminar);
+            Map(x => x.DateCreated);
         }
     }
 }
--- Agribusiness.DB/Schema Objects/Schemas/dbo/Tables/Templates.table.sql
+++ Agribusiness.DB/Schema Objects/Schemas/dbo/Tables/Templates.table.sql
@@ -4,6 +4,7 @@
     [IsActive]           BIT           NOT NULL,
     [NotificationTypeId] CHAR (2)      NOT NULL, 
     [SeminarId] INT NOT NULL, 
+    [DateCreated] DATETIME NOT NULL DEFAULT getdate(), 
     CONSTRAINT [FK_Templates_Seminars] FOREIGN KEY ([SeminarId]) REFERENCES [Seminars]([Id])
 );
 
