6f6d51edbe340ad8705d1e85b2281674a8b1116c
Alan Lai <anlai@ucdavis.edu>
2013-05-06 11:06:36 -0700

    added new query to export all

--- Agribusiness.DB/Scripts/ExportPeople.sql
+++ Agribusiness.DB/Scripts/ExportPeople.sql
@@ -14,3 +14,21 @@ from people
 	left outer join firms on SeminarPeople.firmid = firms.id
 where people.id in (select personid from peoplexsites where siteid = @site)
   and seminarpeople.id = ( select max(sp.id) from seminarpeople sp where sp.personid = people.id )
+
+
+
+/*
+	Generates list of all people in the database
+*/
+select p.id personid, p.firstname, p.mi, p.lastname
+	, sp.Title, s.[Year], s.SiteId
+	, am.loweredemail
+	, bad.line1, bad.line2, bad.city, bad.[state], bad.zip, bad.addresstypeid, bad.countryid
+	, cad.line1, cad.line2, cad.city, cad.[state], cad.zip, cad.addresstypeid, cad.countryid
+from people p
+	left outer join SeminarPeople sp on p.id = sp.PersonId
+	left outer join seminars s on s.id = sp.seminarid
+	inner join aspnet_Membership am on am.userid = p.userid
+	left outer join addresses bad on p.id = bad.personid and bad.addresstypeid = 'B'
+	left outer join addresses cad on p.id = cad.personid and cad.AddressTypeId = 'C'
+order by p.lastname
