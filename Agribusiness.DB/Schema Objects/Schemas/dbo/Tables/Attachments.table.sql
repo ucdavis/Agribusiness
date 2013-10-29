8f92bc644940b52de78fde28126308870157d511
Jason Sylvestre <jsylvestre@ucdavis.edu>
2013-10-29 10:49:08 -0700

    Increased the content type field size. manually made change on donbot

--- Agribusiness.DB/Schema Objects/Schemas/dbo/Tables/Attachments.table.sql
+++ Agribusiness.DB/Schema Objects/Schemas/dbo/Tables/Attachments.table.sql
@@ -3,5 +3,5 @@
 	[Id]			int				IDENTITY(1,1) NOT NULL, 
 	[Contents]		varbinary(max)	NOT NULL,
 	[FileName]		varchar(100)	NOT NULL,
-	[ContentType]	varchar(50)		NOT NULL
+	[ContentType]	varchar(150)		NOT NULL
 )
