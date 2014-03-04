4a907a02d42355217615bea68f2fd82fec6ec295
Scott Kirkland <srkirkland@gmail.com>
2014-03-04 15:41:00 -0800

    we dont need no stinkin backups

--- Backup/Agribusiness.Import/Models/Tracking.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Agribusiness.Import.Models
-{
-    public class Tracking : DomainObject
-    {
-        public string Name { get; set; }
-    }
-}
--- Backup/Agribusiness.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Agribusiness.Web.MvcApplication" Language="C#" %>
